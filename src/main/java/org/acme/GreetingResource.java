88e909409ca98a9b0ea3f51e769947708f7c2da1
Mohammadi Iram <mohammadiiram@gmail.com>
2021-12-07 18:08:39 +0530

    second commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,59 @@
 package org.acme;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/blog")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String all() {
+        return "Read all Blogs";
     }
+
+
+    @GET
+    @Path("/getallblog")
+
+    public String getallblog () {
+        return "You are Viewing the list of all the blogs";
+    }
+
+
+    @POST
+    @Path("/postblog")
+    public String postblog() {
+        return "A new Blog is created with content Hello";
+    }
+
+//    @GET
+//    @Path("/getparblog")
+//    public String getparblog () {
+//        return "You are viewing a blog with the id 12";
+//    }
+
+    @GET
+    @Path("/{name}/{id}")
+    public String getparblog (@PathParam("name") String name, @PathParam("id")String id) {
+        return "You are viewing a blog with the id " +name   +id;
+    }
+
+    @PUT
+    @Path("/putblog/{id}/{name}")
+    public String putblog (@PathParam("id") String name, @PathParam("name")String id) {
+        return "The blog has been edited with the 67 "+id   +name ;
+    }
+
+
+
+
+    @DELETE
+    @Path("/deleteblog")
+    public String deleteblog (@PathParam("id") String id){
+        return "You deleted the blg with the id" +89;
+    }
+
+
+
 }
